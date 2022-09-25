d3cbbca882a7dfce6c5f84debe9830e23e2ff987
Fabian Diez <fabian.diez@otto.de>
2022-09-25 19:04:10 +0200

    Convert project to spring boot cloud function

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.transformers.*
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
 	id("org.springframework.boot") version "2.7.4"
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
+
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -16,14 +20,29 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+	implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+	implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
+
+	implementation("org.springframework.cloud:spring-cloud-function-web:3.2.7")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.4")
+	implementation("org.springframework.cloud:spring-cloud-function-adapter-aws:3.2.7")
+	implementation("org.springframework.boot:spring-boot-starter-webflux:2.7.3")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.3")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.4")
+	}
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,4 +54,31 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+	manifest {
+		attributes["Start-Class"] = "tech.fdiez.flowfullybackend.FlowfullyBackendApplication"
+	}
+}
+
+tasks.assemble {
+	dependsOn("shadowJar")
+}
+
+
+tasks.withType<ShadowJar> {
+	archiveFileName.set("flowfullyBackendApplication.jar")
+	dependencies {
+		exclude("org.springframework.cloud:spring-cloud-function-web")
+	}
+	// Required for Spring
+	mergeServiceFiles()
+	append("META-INF/spring.handlers")
+	append("META-INF/spring.schemas")
+	append("META-INF/spring.tooling")
+	transform(PropertiesFileTransformer::class.java) {
+		paths.add("META-INF/spring.factories")
+		mergeStrategy = "append"
+	}
 }
--- src/main/kotlin/tech/fdiez/flowfullybackend/FlowfullyBackendApplication.kt
+++ src/main/kotlin/tech/fdiez/flowfullybackend/FlowfullyBackendApplication.kt
@@ -2,10 +2,18 @@ package tech.fdiez.flowfullybackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class FlowfullyBackendApplication
+class FlowfullyBackendApplication {
+
+    @Bean
+    fun greet(): (String) -> String {
+        return { input -> "Hello from kotlin $input" }
+    }
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<FlowfullyBackendApplication>(*args)
+    runApplication<FlowfullyBackendApplication>(*args)
 }
